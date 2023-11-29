d069dc5662a0c4fe65540abac01cbf5c59109129
alejandramunoze <atm92@georgetown.edu>
2023-11-28 23:57:29 -0500

    First save and basic creation of project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # employment-learning
 Following the video tutorial from https://www.youtube.com/watch?v=yBDHkveJUf4
+
+
+# Remember to:
+conda activate my-first-env
+
---
+++ app.py
@@ -0,0 +1,18 @@
+
+
+from flask import Flask
+
+# Create a flask application
+app = Flask(__name__) # must give it a name
+
+# Registrating a route
+
+@app.route("/") # empty page
+def home():
+    return "Welcome to Hoya Hunters"
+
+
+# With the following we can use "python app.py" to get it running without having to use the "export FLASK_APP=app, flask run" command
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',debug=True)
+
