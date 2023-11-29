c47e7c0f05bd7c74c0270a9aed2f48a70208e2b9
alejandramunoze <atm92@georgetown.edu>
2023-11-29 00:15:45 -0500

    little update

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ app = Flask(__name__) # must give it a name
 
 @app.route("/") # empty page
 def home():
-    return "Welcome to Hoya Hunters"
+    return render_template('home.html') #In the function we render the formatting from html
 
 
 # With the following we can use "python app.py" to get it running without having to use the "export FLASK_APP=app, flask run" command
